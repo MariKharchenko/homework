dc11b103ab3f2d6342fa4cdf359ac89c90af6cb1
Maryna <109352903+MariKharchenko@users.noreply.github.com>
2023-06-02 14:34:26 +0300

    Add files via upload

---
+++ homework1.js
@@ -0,0 +1,72 @@
+// Task 1: Напишіть скріпт всередині HTML-документу, який виводить на екран ваше імя за допомогою функції alert().
+// Закоментовано в index.html
+
+// Task 2: Напишіть скріпт в зовнішньому файлі, який виводить в консоль ваше прізвище. Підключіть створений файл до HTML-документу. 
+
+// const surname = 'Kharchenko'
+// console.log(surname);
+
+// Task 3: a) оголосіть дві змінні; b) запишіть у змінні будь-які значення; c) виведіть на екран значення змінних; 
+//d) скопіюйте значення однієї змінної в іншу; e) виведіть на екран значення змінних. 
+
+// let name = 'Maryna';
+// let surname = 'Kharchenko';
+// console.log(name + ' ' + surname);
+// surname = name;
+// console.log(name + ' ' + surname);
+
+// Task 4: Створіть об’єкт з 5-ма наступними властивостями: “String”, “Number”, “Boolean”, “Undefined”, “Null”. 
+//Значення кожної властивості повинно відповідати відповідному примітивному типу даних. 
+
+// const client = {
+// name: 'Maryna Kharchenko',
+// orderNumber: 20000,
+// orderPaid: true,
+// orderStatus: undefined,
+// personalDiscount: null
+// }
+// console.log(client);
+
+// Task 5: Використовуючи функцію confirm() задайте користувачу питання про досягнення ним повнолітнього віку. 
+//Результат запишіть в змінну isAdult і виведіть в консоль.
+
+// let isAdult = confirm('Are you over 18 years old?');
+// console.log(isAdult);
+
+// Task 6:
+// let name = 'Maryna';
+// let surname = 'Kharchenko';
+// let studyGroup = 1;
+// let yearOfBirth = 1993;
+// let maritalStatus = confirm('Hi, ' + name + ', are you married?');
+// console.log(typeof studyGroup, typeof yearOfBirth, typeof maritalStatus, typeof name, typeof surname);
+// let hobbies;
+// let additionalInfo = null;
+// console.log(typeof hobbies, typeof homeworkPassed);
+
+// Task 7: За допомогою функції prompt() напишіть скріпт, який послідовно запитує в користувача логін, 
+// емейл та пароль, і виводить на екран повідомлення із введеними даними. 
+// Наприклад “Dear User, your email is usermale@gmail.com, your password is qwerty”. 
+
+// let login = prompt('Hey there :) please enter you login:');
+// let email = prompt('Cool! Now enter your email:');
+// let password = prompt('Almost there! Please, enter your password: ')
+// alert("Your login is " + login + ', your email is ' + email + ' and your password is ' + password);
+
+// Task 8: Напишіть скріпт, який вираховує кількість секунд в годині, в добі, в місяці, записує результати в відповідні змінні, 
+// і виводить їх значення на екран.
+
+// alert('Hi there!:) Please find some interesting facts regarding timing!');
+// const secondsInMinute = 60;
+// const minutesInHour = 60;
+// const hoursInDay = 12;
+// let daysInJune = 30;
+
+// const hour = secondsInMinute * minutesInHour;
+// const day = hoursInDay * hour;
+// let month = daysInJune * day;
+// alert("Amount of seconds is 1 hour: " + hour + " seconds");
+// alert("Amount os seconds in 1 day: " + day + " seconds");
+// alert("Amount of seconds is June: " + month + " seconds");
+
+
