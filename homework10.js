c80b422566514fc2554f688b6a253a7b4379be5f
Maryna <109352903+MariKharchenko@users.noreply.github.com>
2023-07-11 23:51:35 +0300

    added homework10

---
+++ homework10.js
@@ -0,0 +1,18 @@
+// *************************** Task 1 ************************
+// const os = require('os');
+// const http = require('http');
+
+// http.createServer(function(request, response) {
+// response.writeHead(200, {'Content-Type': 'text/plain'});
+// response.write('Current user name: ' + os.username + '\n');
+// response.write('OS Type: ' + os.type() + '\n');
+// response.write('System work time: ' + os.uptime() + ' minutes' + '\n');
+// response.write('Current work directory: ' + os.homedir() + '\n');
+// response.end();
+// }).listen(5000)
+
+// console.log('Server is running at http://localhost:5000')
+
+// // *************************** Task 2 ************************
+// const helloUser = require("./personalmodule");
+// helloUser();
---
+++ personalmodule.js
@@ -0,0 +1,17 @@
+// // *************************** Task 2 ************************
+
+// let userName = 'Maryna K';
+
+// function helloUser () {
+
+// let currentTimeInUkraine = new Date();
+// let hours = currentTimeInUkraine.getHours();
+// console.log(currentTimeInUkraine);
+// if(hours >= 23 && hours < 5) {console.log('Good Night, ' + userName)}
+// else if (hours >= 5 && hours < 11) {console.log('Good Morning, ' + userName)}
+// else if (hours >= 11 && hours < 17) {console.log('Good Afternoon, ' + userName)}
+// else if (hours >=17 && hours < 23) {console.log('Good Evening, ' + userName)};
+
+// }
+
+// module.exports = helloUser;
